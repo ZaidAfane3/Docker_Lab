929c4cdd72b35e386a87f2e07de795dd69ef2a77
Zaid Afaneh <afaneh.zaid@ieee.org>
2020-07-05 13:36:18 +0300

    Add templates to Flask

--- Flask/Project_01/web_app.py
+++ Flask/Project_01/web_app.py
@@ -1,7 +1,13 @@
-from flask import Flask 
+from flask import Flask, render_template 
+from os import path 
+print (path.isdir('./templates/'))
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello ():
+    if path.isdir('./templates/'):
+        return render_template('index.html')
     return "Hello_World_01"
+
 app.run(debug=True)
